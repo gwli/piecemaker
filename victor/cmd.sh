ab84755455b4920fe745026cdc7ec63a2e3e6537
Victor Li <gwli@outlook.com>
2018-11-03 14:09:12 +0800

    use convert

--- victor/cmd.sh
+++ victor/cmd.sh
@@ -1,1 +1,9 @@
+TOP=`pwd`/..
+
+cd $TOP
+
+rm -fr _pieces
+python piece_maker.py images/car.jpg 4 4
+cd _pieces/car
 montage *.png   # to pieces_outline.png
+ convert piece_0_[01].png +append \( piece_1_[01].png +append \) -append result.png
