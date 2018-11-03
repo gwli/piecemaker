81c666fe3966d616792179e2a7d37901fe684a44
Victor Li <gwli@outlook.com>
2018-11-03 14:22:49 +0800

    use 3x3

--- victor/cmd.sh
+++ victor/cmd.sh
@@ -3,7 +3,7 @@ TOP=`pwd`/..
 cd $TOP
 
 rm -fr _pieces
-python piece_maker.py images/car.jpg 4 4
-cd _pieces/car
-montage *.png   # to pieces_outline.png
- convert piece_0_[01].png +append \( piece_1_[01].png +append \) -append result.png
+python piece_maker.py ~/momo.jpg 3 3
+cd _pieces/momo
+#montage *.png   # to pieces_outline.png
+convert piece_0_[012].png +append \( piece_1_[012].png +append \)  \( piece_2_[012].png +append \) -append result.png
